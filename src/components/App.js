172e526c0c869bba4754bc810a649c65a2c8d1b3
Mireia Maldonado Parra <mirmalparra@gmail.com>
2022-11-28 09:13:21 +0100

    Add fetch

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,29 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import '../styles/core/reset.scss';
 import '../styles/App.scss';
-import data from "../data/adalabers.json";
+// import data from "../data/adalabers.json";
+import fetchAdalabers from "../services/api";
   
 function App() {
-  const [adalabers, setAdalabers] = useState(data);
+  const [adalabers, setAdalabers] = useState([]);
 
-  
-
-  const htmlData = adalabers.results
+  const htmlData = adalabers
   .map((adalaber) => {
     return (<tr><td>{adalaber.name}</td><td>{adalaber.counselor}</td><td>{adalaber.speciality}</td></tr>)
   }
   )
 
+  useEffect(() => {
+    fetchAdalabers().then((data) => {
+      setAdalabers(data.results);
+    });
+  }, []);
+
   return (
     <div className="App">
+      <header>
+        <h1>Adalabers</h1>
+      </header>
         <main>
           <table className="table">
           <thead><tr>
